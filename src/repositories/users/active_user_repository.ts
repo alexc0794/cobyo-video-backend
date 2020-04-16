ca81bde5424884dbc992d84545ce5789d06be03a
Alex Chou <alexchou94@gmail.com>
2020-04-15 22:28:09 -0400

    Dont query activeusers table

--- src/repositories/users/active_user_repository.ts
+++ src/repositories/users/active_user_repository.ts
@@ -6,39 +6,41 @@ export default class ActiveUserRepository extends BaseRepository {
   tableName = 'ActiveUsers';
 
   async getActiveUsers(): Promise<Array<ActiveUser>> {
-    return new Promise((resolve, reject) =>
-      this.awsClient.scan({
-        TableName: this.tableName
-      }, (err, data) => {
-        if (err) {
-          console.error(`Could not find active users`);
-          return resolve([]);
-        }
-        return resolve(data.Items);
-      })
-    );
+    return Promise.resolve([]);
+    // return new Promise((resolve, reject) =>
+    //   this.awsClient.scan({
+    //     TableName: this.tableName
+    //   }, (err, data) => {
+    //     if (err) {
+    //       console.error(`Could not find active users`);
+    //       return resolve([]);
+    //     }
+    //     return resolve(data.Items);
+    //   })
+    // );
   }
 
   async updateActiveUser(userId: string): Promise<boolean> {
-    const SECONDS_IN_A_MINUTE = 60;
-    const secondsSinceEpoch = Math.round(Date.now() / 1000);
-    const item = {
-      userId,
-      lastActiveAt: (new Date()).toISOString(),
-      expiringAtSeconds: secondsSinceEpoch + (SECONDS_IN_A_MINUTE * 2)
-    };
-    return new Promise((resolve, reject) =>
-      this.awsClient.put({
-        TableName: this.tableName,
-        Item: item
-      }, (err, data) => {
-        if (err) {
-          console.error('Failed to update active user', userId, err);
-          return resolve(false);
-        }
-        return resolve(true);
-      })
-    );
+    return Promise.resolve(false);
+    // const SECONDS_IN_A_MINUTE = 60;
+    // const secondsSinceEpoch = Math.round(Date.now() / 1000);
+    // const item = {
+    //   userId,
+    //   lastActiveAt: (new Date()).toISOString(),
+    //   expiringAtSeconds: secondsSinceEpoch + (SECONDS_IN_A_MINUTE * 2)
+    // };
+    // return new Promise((resolve, reject) =>
+    //   this.awsClient.put({
+    //     TableName: this.tableName,
+    //     Item: item
+    //   }, (err, data) => {
+    //     if (err) {
+    //       console.error('Failed to update active user', userId, err);
+    //       return resolve(false);
+    //     }
+    //     return resolve(true);
+    //   })
+    // );
   }
 
 }
