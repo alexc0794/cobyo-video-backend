8beb6b423e03828123a3fcca60660e5920fe43b6
Alex Chou <alexchou94@gmail.com>
2020-04-18 20:42:20 -0400

    Enable purchaes of items to multiple ppl

--- src/routes/storefront/menu.ts
+++ src/routes/storefront/menu.ts
@@ -43,49 +43,53 @@ export default (app: Router) => {
 
   type PurchaseFromMenuResponse = {
     updatedWalletInCents: number,
-    inventoryItem: InventoryItem,
     toUserIds: Array<string>,
   };
 
   // TODO: Authenticate instead of pass fromUserId from body
-  app.post('/menu/purchase', async function (req: any, res) {
+  app.post('/menu/purchase', async function(req: any, res) {
     const fromUserId: string = req.body.fromUserId;
     const itemId: string = req.body.itemId;
-    const toUserId: string = req.body.toUserId || fromUserId;
-    const toUserIds: Array<string> = [toUserId]; // TODO: Implement purchase for multiple ppl
-    const menuItem: MenuItem|null = (new MenuRepository()).getMenuItemById(itemId)
+    const toUserId: string | null = req.body.toUserId;
+    const toUserIds: Array<string> = toUserId ? [toUserId] : (req.body.toUserIds || []);
+    const numItemsToPuchase = toUserIds.length;
+    const menuItem: MenuItem | null = (new MenuRepository()).getMenuItemById(itemId)
     if (!menuItem) {
       return res.sendStatus(500);
     }
 
     let updatedWalletInCents: number;
     try {
-      updatedWalletInCents = await (new UserRepository()).updateUserWallet(fromUserId, -1 * menuItem.cents);
+      updatedWalletInCents = await (new UserRepository()).updateUserWallet(fromUserId, -1 * menuItem.cents * numItemsToPuchase);
       if (updatedWalletInCents < 0) {
-        console.warn('Shouldnt have gotten to a state where user has negative amount in wallet');
         // Revert the update if user didnt have enough money
-        updatedWalletInCents = await (new UserRepository()).updateUserWallet(fromUserId, menuItem.cents);
-        return res.status(400).send({ updatedWalletInCents, inventoryItem: null });
+        updatedWalletInCents = await (new UserRepository()).updateUserWallet(fromUserId, menuItem.cents * numItemsToPuchase);
+        return res.status(400).send({ updatedWalletInCents });
       }
     } catch {
       return res.sendStatus(500);
     }
 
-    let inventoryItem: InventoryItem;
-    try {
-      inventoryItem = await (new UserInventoryRepository()).addItemToInventory(
-        toUserId, // Send to toUserId
-        itemId,
-        fromUserId,
-        menuItem.expirationInSeconds,
-      );
-    } catch {
-      // Add money back to user
-      updatedWalletInCents = await (new UserRepository()).updateUserWallet(fromUserId, menuItem.cents);
-      return res.status(400).send({ updatedWalletInCents, inventoryItem: null });
-    }
+    const sentToUserIds: Array<string> = [];
+    toUserIds.forEach(async (toUserId: string) => {
+      try {
+        await (new UserInventoryRepository()).addItemToInventory(
+          toUserId,
+          itemId,
+          fromUserId,
+          menuItem.expirationInSeconds,
+        );
+        sentToUserIds.push(toUserId);
+      } catch {
+        // Add money back to user
+        updatedWalletInCents = await (new UserRepository()).updateUserWallet(fromUserId, menuItem.cents);
+      }
+    });
 
-    const response: PurchaseFromMenuResponse = { updatedWalletInCents, inventoryItem, toUserIds };
+    const response: PurchaseFromMenuResponse = {
+      updatedWalletInCents,
+      toUserIds: sentToUserIds,
+    };
     return res.send(response);
   });
 }
